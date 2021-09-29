32b153f971ef059a3b36041abafcbbb9750910d7
Jose Palomo <josepalriv@gmail.com>
2021-09-29 11:17:25 -0300

    se configuran validaciones para el pago

--- centroMedico/src/app/pago/pago.page.ts
+++ centroMedico/src/app/pago/pago.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pago',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pago.page.scss'],
 })
 export class PagoPage implements OnInit {
+  cardnumber: string = "";
+  fullname: string = "";
+  fvencimiento: string = "";
+  cvc: string = "";
 
-  constructor() { }
+
+
+  constructor(public alerta: AlertController, private navCtrl:NavController) { }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+     
+    if(this.cardnumber=="" || this.fullname =="" || this.fvencimiento=="" || this.cvc==""){
+
+          this.validacionFormulario();
+
+    }
+    else{
+
+      this.navCtrl.navigateForward("login");
+
+    }
+  }
+  async validacionFormulario() {
+    const alert = await this.alerta.create({
+      cssClass: 'my-custom-class',
+      header: 'Error',
+      //subHeader: 'Subtitle',
+      message: 'Por favor ingresar los campos que faltan.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
 }
