d5713247bb01b888d9d28ad5d980dca3f5efee18
Jose Palomo <josepalriv@gmail.com>
2021-09-28 21:55:25 -0300

    Creación de página de pago

--- centroMedico/src/app/app-routing.module.ts
+++ centroMedico/src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'agendar-hora',
     loadChildren: () => import('./agendar-hora/agendar-hora.module').then( m => m.AgendarHoraPageModule)
   },
+  {
+    path: 'pago',
+    loadChildren: () => import('./pago/pago.module').then( m => m.PagoPageModule)
+  },
 
 ];
 
