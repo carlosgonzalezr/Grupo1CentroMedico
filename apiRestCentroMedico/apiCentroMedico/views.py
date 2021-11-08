bde50d2e7c109f41fcb6b6f7877fb293cd5a5b48
Jose Palomo <jo.palomo@duocuc.cl>
2021-11-08 10:22:26 -0300

    Se crea modelo paciente y se añade a path admin

--- apiRestCentroMedico/apiCentroMedico/views.py
+++ apiRestCentroMedico/apiCentroMedico/views.py
@@ -3,14 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions, status
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from .models import Paciente
+from .serializers import UserSerializer, PacienteSerializer
 # Create your views here.
-class HelloView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request):
-        content = {'message' : 'Hola Mundo!'}
-        return Response(content)
 
 class ListUser(APIView):
     authentication_classes = [authentication.TokenAuthentication]
@@ -46,4 +41,40 @@ class ListUser(APIView):
     def delete(self, request, id=None):
         item = get_object_or_404(User, id=id)
         item.delete()
-        return Response({"status": "success", "data": "User Deletedd"})
+        return Response({"status": "success", "data": "User Deleted"})
+
+class ListPaciente(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
+
+    def post(self, request):
+        serializer = PacienteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id=None):
+        item = Paciente.objects.get(correo=id)
+        serializer = PacienteSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data})
+        else:
+            return Response({"status": "error", "data": serializer.errors})
+
+    def get(self, request, id=None):
+        if id:
+            item = Paciente.objects.get(correo=id)
+            serializer = PacienteSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        
+        items = Paciente.objects.all()
+        serializer = PacienteSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    def delete(self, request, id=None):
+        item = get_object_or_404(Paciente, correo=id)
+        item.delete()
+        return Response({"status": "success", "data": "User Deleted"})
