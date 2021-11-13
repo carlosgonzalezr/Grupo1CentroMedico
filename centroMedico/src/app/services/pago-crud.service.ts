424dc37f8c8c92611f53ec33ce3545d643caee8b
carlos gonzalez <car.gonzalez3@duocuc.cl>
2021-11-12 22:31:48 -0300

    cambioscarlos

--- centroMedico/src/app/services/pago-crud.service.ts
+++ centroMedico/src/app/services/pago-crud.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
